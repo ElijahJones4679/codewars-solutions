b299209dc800cc8e394a40a53b89c373b6728c42
elijah jones <jones.elijah4679@gmail.com>
2024-01-12 00:22:03 -0600

    second commit

---
+++ 8kyu/C#/Exclamation-Mark-Series-#2.cs
@@ -0,0 +1,12 @@
+//              Instructions
+/* Remove all exclamation marks from the end of sentence.*/
+
+//              Solution
+
+public class Kata
+{
+    public static string Remove(string s) 
+    {
+        return s.TrimEnd('!');   //coding and coding..  
+    }                 
+}
