e3ee7c5a63dad4873963b5f5aa8dadeae15afa44
elijah jones <jones.elijah4679@gmail.com>
2024-01-15 13:11:15 -0600

    third commit update

--- 8kyu/Javascript/Even-Or-Odds.js
+++ 8kyu/Javascript/Even-Or-Odds.js
@@ -3,7 +3,7 @@
 //that takes an integer as an 
 //argument and returns "Even" for 
 //even numbers or "Odd" for odd numbers.
-
+//codewars: link - https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/javascript
 //                      Solutions
 
 function evenOrOdd(number) 
