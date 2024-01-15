80be5c788cfbe3f20f2750a9662112472a3de908
elijah jones <jones.elijah4679@gmail.com>
2024-01-15 13:11:53 -0600

    fourth commit update

--- 8kyu/Javascript/Exclamation-Mark-Series-#2.js
+++ 8kyu/Javascript/Exclamation-Mark-Series-#2.js
@@ -1,6 +1,6 @@
 //              Instructions
 /* Remove all exclamation marks from the end of sentence.*/
-
+//codewars: link - https://www.codewars.com/kata/57faece99610ced690000165/javascript
 //              Solution
 
 function remove (string) 
